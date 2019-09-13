f043ae4a63e05b901306a21b9a68be2c70834226
Fabio Landi <fabio.landi@mercadolivre.com>
2019-09-13 20:10:55 -0300

    release 4.0.8 and basic enable fixed

--- includes/payments/WC_WooMercadoPago_BasicGateway.php
+++ includes/payments/WC_WooMercadoPago_BasicGateway.php
@@ -179,7 +179,7 @@ class WC_WooMercadoPago_BasicGateway extends WC_WooMercadoPago_PaymentAbstract
             return true;
         }
 
-        if ($this->settings['enabled'] == 'yes') {
+        if (isset($this->settings['enabled']) && $this->settings['enabled'] == 'yes') {
             if ($this->mp instanceof MP) {
                 $accessToken = $this->mp->get_access_token();
                 if (strpos($accessToken, 'APP_USR') === false && strpos($accessToken, 'TEST') === false) {
