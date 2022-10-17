0f0d779538527caac545e467a0747ba55d9def09
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2022-10-17 15:07:30 -0300

    feat: added autoloader

---
+++ includes/Autoloader.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MercadoPago\CartWooCommerce\Autoloader;
+
+defined('ABSPATH') || exit;
+
+class Autoloader
+{
+
+    public static function init()
+    {
+        $autoloader = dirname(__DIR__) . '../vendor/autoload.php';
+
+        if (! is_readable($autoloader)) {
+            self::missingAutoload();
+            return false;
+        }
+
+        $autoloader_result = require $autoloader;
+
+        if (!$autoloader_result) {
+            return false;
+        }
+
+        return $autoloader_result;
+    }
+
+    /**
+     * @return void
+     */
+    protected static function missingAutoload()
+    {
+    }
+}
