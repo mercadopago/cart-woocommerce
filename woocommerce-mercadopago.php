499f5d10be94c0a7240334cf93473dae724be418
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2022-10-17 14:48:00 -0300

    feat: init folders structure

---
+++ woocommerce-mercadopago.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Mercado Pago payments for WooCommerce
+ * Plugin URI: https://github.com/mercadopago/cart-woocommerce
+ * Description: Configure the payment options and accept payments with cards, ticket and money of Mercado Pago account.
+ * Version: 8.0.0
+ * Author: Mercado Pago
+ * Author URI: https://developers.mercadopago.com/
+ * Text Domain: woocommerce-mercadopago
+ * Domain Path: /i18n/languages/
+ * WC requires at least: 5.5.2
+ * WC tested up to: 6.3.0
+ * Requires PHP: 7.2
+ *
+ * @package MercadoPago
+ */
