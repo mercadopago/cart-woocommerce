627184fc38203b6f80940bf776e6fc2eac349d75
hugodamm <hugo2008rj@hotmail.com>
2022-10-24 18:56:32 -0300

    File uninstall implementation v2

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,8 @@
 <?php
 
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-    exit;
-}
-
-delete_option( 'woocommerce-mercadopago' );
-delete_site_option( 'woocommerce-mercadopago' );
+//if (!defined('WP_UNINSTALL_PLUGIN')) {
+//    exit;
+//}
+//
+//delete_option( 'woocommerce-mercadopago' );
+//delete_site_option( 'woocommerce-mercadopago' );
