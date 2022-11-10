bec55ad09f818ab5b61c707012198dcb0420509b
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2022-11-10 20:05:26 -0300

    feat: improvements

--- src/WoocommerceMercadoPago.php
+++ src/WoocommerceMercadoPago.php
@@ -163,9 +163,6 @@ class WoocommerceMercadoPago
         if (!class_exists('WC_Payment_Gateway')) {
             $this->notices->adminNoticeMissWoocoommerce();
         }
-
-        $this->logs->file->info('Test file log on init', self::class);
-        $this->logs->remote->info('Test remote log on init', self::class);
     }
 
     /**
