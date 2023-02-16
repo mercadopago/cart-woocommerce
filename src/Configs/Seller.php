ac3a510e79414089df3aba8f38969b0fe5fab1b6
Gustavo Brito <gustavo.brito@mercadolivre.com>
2023-02-16 11:45:44 -0300

    fix: fixing paycash payment places

--- src/Configs/Seller.php
+++ src/Configs/Seller.php
@@ -470,7 +470,63 @@ class Seller
             $serializedPaymentMethods[] = $paymentMethod;
         }
 
+        $serializedPaymentMethods = $this->buildPaymentPlaces($serializedPaymentMethods);
+
         $this->setCheckoutTicketPaymentMethods($serializedPaymentMethods);
+    }
+
+    /**
+     * Build payment places for paycash
+     *
+     * @param array $serializedPaymentMethods
+     *
+     * @return array
+     */
+    public function buildPaymentPlaces(array $serializedPaymentMethods): array
+    {
+        $payment_places =
+			[
+				'paycash' => [
+					[
+						'payment_option_id' => '7eleven',
+						'name'              => '7 Eleven',
+						'status'            => 'active',
+						'thumbnail'         => 'https://http2.mlstatic.com/storage/logos-api-admin/417ddb90-34ab-11e9-b8b8-15cad73057aa-s.png'
+					],
+					[
+						'payment_option_id' => 'circlek',
+						'name'              => 'Circle K',
+						'status'            => 'active',
+						'thumbnail'         => 'https://http2.mlstatic.com/storage/logos-api-admin/6f952c90-34ab-11e9-8357-f13e9b392369-s.png'
+					],
+					[
+						'payment_option_id' => 'soriana',
+						'name'              => 'Soriana',
+						'status'            => 'active',
+						'thumbnail'         => 'https://http2.mlstatic.com/storage/logos-api-admin/dac0bf10-01eb-11ec-ad92-052532916206-s.png'
+					],
+					[
+						'payment_option_id' => 'extra',
+						'name'              => 'Extra',
+						'status'            => 'active',
+						'thumbnail'         => 'https://http2.mlstatic.com/storage/logos-api-admin/9c8f26b0-34ab-11e9-b8b8-15cad73057aa-s.png'
+					],
+					[
+						'payment_option_id' => 'calimax',
+						'name'              => 'Calimax',
+						'status'            => 'active',
+						'thumbnail'         => 'https://http2.mlstatic.com/storage/logos-api-admin/52efa730-01ec-11ec-ba6b-c5f27048193b-s.png'
+					],
+				],
+			];
+
+		foreach ( $serializedPaymentMethods as $key => $method ) {
+			if (isset( $payment_places[$method['id']])) {
+				$serializedPaymentMethods[$key]['payment_places'] = $payment_places[$method['id']];
+			}
+		}
+
+        return $serializedPaymentMethods;
     }
 
     /**
