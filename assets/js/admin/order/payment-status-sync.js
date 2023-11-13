1fa923b63693332697b414fde91816b9172b3a97
Gustavo Brito <gustavo.brito@mercadolivre.com>
2023-11-13 09:15:48 -0300

    fix(7.0.0): moving status sync up

--- assets/js/admin/order/payment-status-sync.js
+++ assets/js/admin/order/payment-status-sync.js
@@ -10,8 +10,8 @@
       }
   
       const orderDataElement = document.getElementById('woocommerce-order-data');
-      const paymentStatusMetaboxElement = document.getElementById('mp-payment-status-metabox');
-      const paymentStatusMetaboxTitle = document.querySelector('#mp-payment-status-metabox > div.postbox-header > h2');
+      const paymentStatusMetaboxElement = document.getElementById('mp_payment_status_sync');
+      const paymentStatusMetaboxTitle = document.querySelector('#mp_payment_status_sync > div.postbox-header > h2');
   
       if (orderDataElement && paymentStatusMetaboxElement) {
         orderDataElement.after(paymentStatusMetaboxElement);
