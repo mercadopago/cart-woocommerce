4019ef6681fd08ac2f0a40879d30e36e64ac69bf
DouglasCorreiaMeli <douglas.correia@mercadolivre.com>
2024-01-09 16:05:52 -0300

    running build script

--- build/custom.block.asset.php
+++ build/custom.block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities'), 'version' => '229d798c27af15b35b03');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities'), 'version' => '6a0e7ab668ec26be7e7d');
