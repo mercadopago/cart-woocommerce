8c1f1b1c58de0aa11c16163e166a97f361c69004
DouglasCorreiaMeli <douglas.correia@mercadolivre.com>
2024-01-09 14:38:45 -0300

    fix: :bug: Add helper to handle with cart total changes
    I've created a dedicated coded to handle the logic of updating the cardForm value

--- assets/js/blocks/helpers/cart-update.helper.js
+++ assets/js/blocks/helpers/cart-update.helper.js
@@ -20,4 +20,23 @@ const removeDiscountAndCommission = (callback, paymentMethodName) => {
   });
 };
 
-export { addDiscountAndCommission, removeDiscountAndCommission };
+function formatCurrency(value, currency) {
+  if (!Number.isInteger(value) || typeof currency !== 'object') {
+    throw new Error('Invalid input');
+  }
+  const formattedValue = (value / Math.pow(10, currency.minorUnit)).toFixed(currency.minorUnit);
+  const parts = formattedValue.split('.');
+  const integerPart = parts[0]
+  const decimalPart = parts[1];
+  return `${integerPart}.${decimalPart}`;
+}
+
+function handleCartTotalChange(value, currency){
+  if (cardFormMounted) {
+    cardForm.unmount();
+  }
+  initCardForm(formatCurrency(value, currency));
+}
+
+export { addDiscountAndCommission, handleCartTotalChange, removeDiscountAndCommission };
+
