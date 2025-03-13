26953137ca9ee0641a7b56bd8422b5d63ff364f2
Mercado Pago <woocommerce.dev@mercadopago.com>
2025-03-13 19:22:53 +0000

    Release v7.10.2

--- build/custom.block.asset.php
+++ build/custom.block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities'), 'version' => '107bc6dd7b87cc1295bd');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities'), 'version' => '0640e3beb8b421c1b14d');
--- build/pix.block.asset.php
+++ build/pix.block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities'), 'version' => 'efd0d586f4935728e577');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities'), 'version' => '5b758f27b7d172349745');
